221127b686c118fc7b5329b0aa640f2299362e90
Jordan Read <jread6477@gmail.com>
2021-08-09 15:15:33 -0400

    Add project files.

---
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] Customer customer)
+        {
+            if(customer is null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            
+             
+        }
+    }
+}
---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public string SKU { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int NumberInInventory { get; set; }
+        public bool IsInStock
+        {
+            get
+            {
+                if (NumberInInventory > 0)
+                    return true;
+                else return false;
+            }
+  
+        }
+    }
+}
