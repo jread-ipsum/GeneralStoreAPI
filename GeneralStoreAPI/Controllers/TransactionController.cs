d27339c5f5df7807dd75a56ba61c623e9e172c12
Jordan Read <jread6477@gmail.com>
2021-08-17 17:08:57 -0400

    Transaction Controller bonus get total sales by product sku attempted.

--- GeneralStoreAPI/Controllers/TransactionController.cs
+++ GeneralStoreAPI/Controllers/TransactionController.cs
@@ -62,7 +62,7 @@ namespace GeneralStoreAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IHttpActionResult> GetAll([FromUri] int id)
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
         {
             var transaction = await _context.Transactions.SingleOrDefaultAsync(t => t.Id == id);
             
@@ -71,6 +71,83 @@ namespace GeneralStoreAPI.Controllers
                 return NotFound();
             }
             return Ok(transaction);
+        }
+
+        //Bonus Get All Transactions by CustomerId
+        [HttpGet]
+        [Route("api/Transaction/{customerId}")]
+        public async Task<IHttpActionResult> GetAllByCustomerId([FromUri] int customerId)
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+
+            List<Transaction> customerTransactions = new List<Transaction>();
+
+            foreach(Transaction transaction in transactions)
+            {
+                if(transaction.CustomerId == customerId)
+                {
+                    customerTransactions.Add(transaction);
+                }
+            }
+
+            if(customerTransactions is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customerTransactions);
+
+        }
+
+        //Bonus Get All transactions between a date range
+        [HttpGet]
+        [Route("api/Transaction/{dateA}/{dateB}")]
+        public async Task<IHttpActionResult> GetAllByDateRange([FromUri] DateTime dateA, DateTime dateB)
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+
+            List<Transaction> dateRangeTransactions = new List<Transaction>();
+
+            foreach(Transaction transaction in transactions)
+            {
+                if(transaction.DateOfTransaction.Date >= dateA.Date && transaction.DateOfTransaction.Date <= dateB.Date)
+                {
+                    dateRangeTransactions.Add(transaction);
+                }
+
+            }
+
+            if(dateRangeTransactions is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dateRangeTransactions);
+        }
+
+        //Bonus Get Total Sales by Product SKU
+        [HttpGet]
+        [Route("api/Transaction/{productSku}")]
+        public async Task<IHttpActionResult> GetTotalSalesByProductSku([FromUri] string productSku)
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+
+            if(transactions is null)
+            {
+                NotFound();
+            }
+
+            List<double> costs = new List<double>();
+            var totalSales = costs.Sum();
+            foreach(Transaction transaction in transactions)
+            {
+                if(transaction.ProductSKU == productSku)
+                {
+                    costs.Add(transaction.Product.Cost);
+                }
+            }
+
+            return Ok(totalSales);
         }
     }
 }
